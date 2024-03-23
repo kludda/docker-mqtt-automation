c06fa2cdc75ca2b48240d59b3cf5853b46974f69
kludda <david.alind@gmail.com>
2024-03-23 10:06:44 +0100

    Update install-service.sh

--- install-service.sh
+++ install-service.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-WD="WorkingDirectory=$PWD"
-
-sed -i "|WorkingDirectory|c\$PWD|" ./mqtt-automation.service
-
-#cp mqtt-automation.service /etc/systemd/system/
-#systemctl enable mqtt-automation
-#systemctl start mqtt-automation
+sed '\|WorkingDirectory|c\WorkingDirectory='$PWD ./mqtt-automation.service
+cp mqtt-automation.service /etc/systemd/system/
+systemctl enable mqtt-automation
