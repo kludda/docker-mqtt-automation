f222a0ad795876fb52a29b97d018ae9d744efd3a
kludda <david.alind@gmail.com>
2024-03-23 07:48:55 +0100

    Update update-datasources.sh

--- grafana/update-datasources.sh
+++ grafana/update-datasources.sh
@@ -2,7 +2,7 @@
 
 if [ `docker container ls | grep -c grafana$` -gt 0 ]
 then
-	docker cp ./datasources.yaml telegraf:/etc/grafana/provisioning/datasources/datasources.yaml
+	docker cp ./datasources.yaml grafana:/etc/grafana/provisioning/datasources/datasources.yaml
 else
 	echo "ERROR: The 'grafana' container must be running."
 fi
